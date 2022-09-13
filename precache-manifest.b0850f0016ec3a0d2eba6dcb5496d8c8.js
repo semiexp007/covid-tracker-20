2102cd3fcafd8b97bfe12bc128ba1de63ecf8f34
semiexp07 <shekh85275@gmail.com>
2022-09-13 19:16:41 +0530

    Updates

---
+++ precache-manifest.b0850f0016ec3a0d2eba6dcb5496d8c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a692591e8a0d90b62dd5c691bc7ca5",
+    "url": "/covid-tracker-20/index.html"
+  },
+  {
+    "revision": "f7c4a92b3826d7609507",
+    "url": "/covid-tracker-20/static/css/main.5bc4a00c.chunk.css"
+  },
+  {
+    "revision": "932692b3b3a65fa0155f",
+    "url": "/covid-tracker-20/static/js/2.18883208.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-20/static/js/2.18883208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c4a92b3826d7609507",
+    "url": "/covid-tracker-20/static/js/main.d057409a.chunk.js"
+  },
+  {
+    "revision": "c988693bd53bb753be20",
+    "url": "/covid-tracker-20/static/js/runtime-main.844f382d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-20/static/media/image.d7265326.png"
+  }
+]);
